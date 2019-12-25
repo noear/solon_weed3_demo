61afc1c677d9a08c1873302197d27d2718a22797
noear <noear@live.cn>
2019-12-25 17:19:50 +0800

    Update Config.java

--- demo1_anno/src/main/java/webapp/Config.java
+++ demo1_anno/src/main/java/webapp/Config.java
@@ -17,6 +17,7 @@ public class Config {
         //打印执行代码
         WeedConfig.onExecuteBef((cmd) -> {
             System.out.println(cmd.text);
+            System.out.println(cmd.paramMap());
             return true;
         });
     }
